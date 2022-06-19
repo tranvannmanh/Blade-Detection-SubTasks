972a1943d105578e98988b657bf7a386fb086c9d
Đào Việt Cường <cuongsieunhancsn@gmail.com>
2022-06-19 22:11:06 +0700

    Update

--- Tkinter/tkinter_ui.py
+++ Tkinter/tkinter_ui.py
@@ -1,11 +1,12 @@
 from tkinter import *
 from tkinter import ttk, filedialog
+
+import numpy as np
+import scipy.stats as st
+from PIL import Image, ImageTk
 from scipy import signal
 from skimage.morphology import erosion, dilation
-import scipy.stats as st
-from scipy.ndimage import zoom
-import numpy as np
-from PIL import Image, ImageTk
+
 
 class Root(Tk):
     def __init__(self):
@@ -18,16 +19,16 @@ class Root(Tk):
         self.minsize(self.width, self.height)
 
         # Define a container name 'labelFrame'
-        self.labelFrame = ttk.LabelFrame(self, text = "Open File")
+        self.labelFrame = ttk.LabelFrame(self, text="Open File")
 
         # display labelFrame on grid layout
-        self.labelFrame.grid(column = 0, row = 0, padx=(20, 20), pady=(20,20))
+        self.labelFrame.grid(column=0, row=0, padx=(20, 20), pady=(20, 20))
 
         # self.fileBrowser()
         self.label = ttk.Label(self.labelFrame, text='Choose an image! ')
-        self.button = ttk.Button(self.labelFrame, text = "Browse A File",command = self.fileDialog)
+        self.button = ttk.Button(self.labelFrame, text="Browse A File", command=self.fileDialog)
         self.label.grid(column=1, row=0)
-        self.button.grid(column=2, row = 0)
+        self.button.grid(column=2, row=0)
 
         # Display image
         self.display_image = ttk.Label(self.labelFrame)
@@ -36,33 +37,31 @@ class Root(Tk):
         # self.canvas_img = Canvas(self.labelFrame, width=300, height=300, bg='gray')
         # self.canvas_img.grid(row=1, column=0)
 
-
     def fileDialog(self):
-        filename = filedialog.askopenfilename(initialdir = "C:/Users/", title = "Select A File")
-        
-        self.label.configure(text = 'Opening:   '+filename)
+        filename = filedialog.askopenfilename(initialdir="C:/Users/", title="Select A File")
+
+        self.label.configure(text='Opening:   ' + filename)
 
         img = Image.open(filename)
-        self.image= img
+        self.image = img
         self.image_display(img)
-        print("Image's size: ",img.size)
+        print("Image's size: ", img.size)
 
-
-    def imageEditTools(self, image): #image opened by PIL
+    def imageEditTools(self, image):  # image opened by PIL
         tools = ttk.LabelFrame(self, text='tools')
-        tools.grid(column=11, row=0, padx=(0, 20), pady=(0, 10),)
+        tools.grid(column=11, row=0, padx=(0, 20), pady=(0, 10), )
 
-        zoom_in = ttk.Button(tools,text='Zoom Out', command=lambda : self.imageZoomOut(image))
+        zoom_in = ttk.Button(tools, text='Zoom Out', command=lambda: self.imageZoomOut(image))
         zoom_in.grid(column=0, row=0)
 
-        zoom_out = ttk.Button(tools, text='Zoom In', command=lambda : self.imageZoomIn(image))
+        zoom_out = ttk.Button(tools, text='Zoom In', command=lambda: self.imageZoomIn(image))
         zoom_out.grid(column=0, row=1)
 
-        gray_scale = ttk.Button(tools, text='2Gray', command=lambda : self.image2Gray(image))
+        gray_scale = ttk.Button(tools, text='2Gray', command=lambda: self.image2Gray(image))
         gray_scale.grid(column=0, row=2)
 
-        sobel_edge = ttk.Button(tools, text='sobel', command=lambda : self.sobel_edge_detect(image))
-        sobel_edge.grid(column=0,row=3)
+        sobel_edge = ttk.Button(tools, text='sobel', command=lambda: self.sobel_edge_detect(image))
+        sobel_edge.grid(column=0, row=3)
 
         blured_image = ttk.Button(tools, text='Gauss Blur', command=lambda: self.gaussian_blur(image))
         blured_image.grid(column=0, row=3)
@@ -76,14 +75,14 @@ class Root(Tk):
         dilation = ttk.Button(tools, text='Dilation', command=lambda: self.dilate_img(image))
         dilation.grid(column=0, row=6)
 
-        reset = ttk.Button(tools, text='Reset', command=self.origin_img)
-        reset.grid(column=0, row=7)
-
         opening = ttk.Button(tools, text='Opening', command=lambda: self._opening(image))
-        opening.grid(column=0, row=8)
+        opening.grid(column=0, row=7)
 
         closing = ttk.Button(tools, text='Closing', command=lambda: self._closing(image))
-        closing.grid(column=0, row=9)
+        closing.grid(column=0, row=8)
+
+        reset = ttk.Button(tools, text='Reset', command=self.origin_img)
+        reset.grid(column=0, row=9)
 
     def origin_img(self):
         self.image_display(self.image)
@@ -114,12 +113,12 @@ class Root(Tk):
         print('LOG:. zoom_out')
 
     def gaussian_filter(self, size, nsig=1):
-        x = np.linspace(-nsig, nsig, size+1)
+        x = np.linspace(-nsig, nsig, size + 1)
         ker1d = np.diff(st.norm.cdf(x))
         ker2d = np.outer(ker1d, ker1d)
         return ker2d
 
-    def gaussian_blur(self, image, kernel_size=5):
+    def gaussian_blur(self, image):
         try:
             blured_img = signal.convolve2d(image, self.gaussian_filter(5))
             img = Image.fromarray(blured_img)
@@ -127,12 +126,11 @@ class Root(Tk):
             print('LOG:. blured')
             print('...', img.size)
         except Exception as e:
-            print(e,'\nERROR: Gray scale first')
-        
+            print(e, '\nERROR: Gray scale first')
 
     def sobel_edge_detect(self, image):
-        sobel_x = np.array([[1, 0, -1], [2, 0 ,-2], [1, 0, -1]])
-        sobel_y = np.array([[1, 2, 1], [0, 0 ,0], [-1, -2, -1]])
+        sobel_x = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+        sobel_y = np.array([[1, 2, 1], [0, 0, 0], [-1, -2, -1]])
 
         try:
             image_x = signal.convolve2d(image, sobel_x)
@@ -143,8 +141,7 @@ class Root(Tk):
             self.image_display(img)
             print('LOG:. Sobel applied')
         except Exception as e:
-            print(e,'\nERROR:. Gray scale image first!')
-        
+            print(e, '\nERROR:. Gray scale image first!')
 
     def _erosion(self, image, filter=[]):
         if not filter:
@@ -168,19 +165,19 @@ class Root(Tk):
             self.image_display(erosed)
             print('LOG:. Erosion')
         except Exception as e:
-            print('ERROR:. ',e, '\n\t => Gray scale first')
-        
+            print('ERROR:. ', e, '\n\t => Gray scale first')
+
     def dilate_img(self, image):
         try:
             dilated = self._dilation(image)
             self.image_display(dilated)
             print('LOG:. Dilation')
         except Exception as e:
-            print('ERROR:. ',e, '\n\t => Gray scale first')
+            print('ERROR:. ', e, '\n\t => Gray scale first')
 
     def _opening(self, image):
         try:
-            filter = [[1,0,1],[0,1,0],[1,0,1]]
+            filter = [[1, 0, 1], [0, 1, 0], [1, 0, 1]]
             img = self._erosion(image, filter)
             img = self._dilation(img, filter)
             self.image_display(img)
@@ -190,9 +187,9 @@ class Root(Tk):
 
     def _closing(self, image):
         try:
-            filter = [[1,0,1],[0,1,0],[1,0,1]]
-            img=self._dilation(image, filter)
-            img=self._erosion(img,filter)
+            filter = [[1, 0, 1], [0, 1, 0], [1, 0, 1]]
+            img = self._dilation(image, filter)
+            img = self._erosion(img, filter)
             self.image_display(img)
             print('LOG:. Closing')
         except Exception as e:
@@ -201,19 +198,12 @@ class Root(Tk):
     def image_display(self, image):
         img_display = ImageTk.PhotoImage(image)
         self.display_image.configure(image=img_display)
-        self.display_image.image=img_display
+        self.display_image.image = img_display
         self.imageEditTools(image)
+
 
 root = Root()
 root.mainloop()
-
-
-
-
-
-
-
-
 
 # from tkinter import *
 # from tkinter import ttk
